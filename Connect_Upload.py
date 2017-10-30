673c185fbd4911b274d7b493dfface7cd4a96241
Geoffrey Kirk <geoffrey_kirk@trimble.com>
2017-10-30 10:38:32 -0600

    Added support for GLOBed uploads
    Allows you to upload files of only a given file spec. Especially useful
    for when you are using --delete to archive files to connect

--- Connect_Upload.py
+++ Connect_Upload.py
@@ -19,16 +19,17 @@ logger = logging.getLogger(__name__)
 from Connect import Connect
 
 def create_arg_parser():
-    usage="Connect_Upload.py [user] [password] <Project_Name> <Folder>"
+    usage="Connect_Upload.py [user] [password] <Project_Name> <Files or Pattern>"
     parser=argparse.ArgumentParser()
     parser.add_argument("project", type=str, help="Project Name")
     parser.add_argument("folder", type=str, help="Folder in Project")
-    parser.add_argument("files", nargs="+", help="Files to upload")
+    parser.add_argument("files", nargs="*", help="Files to upload")
     parser.add_argument("-T", "--tell",action="store_true", dest="tell", default=False, help="Tell the settings for the run")
     parser.add_argument("-l", "--location",type=str, dest="location", default="us", help="The Connect pod location. (us,europe,asia) Default=us.")
     parser.add_argument("-u", "--user", required=True,type=str, dest="user", help="The Connect username.")
     parser.add_argument("-p", "--password", required=True,type=str, dest="password", help="The Connect password.")
     parser.add_argument("-d", "--delete", action="store_true", dest="delete", help="Delete after file upload. (No Cache) Delete file if exists in connect already (cached)")
+    parser.add_argument("-g", "--glob",type=str, dest="glob", help="File search pattern. On linux make sure you have \" \" around the glob")
     parser.add_argument("-n", "--no-cache", action="store_true", dest="no_cache", help="Delete after file upload")
     parser.add_argument('-v', '--verbose', action='count', default=0,
                    help='increase output verbosity (use up to 3 times)')
@@ -44,23 +45,31 @@ def process_arguments ():
     LOCATION=options.location
     FOLDER=options.folder
     FILES=options.files
+    GLOB=options.glob
     DELETE=options.delete
     CACHE=not options.no_cache
+    
+    if GLOB and FILES != []:
+        sys.exit("Error: You can not use GLOB and provide FILES at the same time")
+
+    if GLOB ==None and FILES==[]:
+        sys.exit("Error: You must provide either GLOB or FILES")
 
     if options.tell:
         print "Project: " + PROJECT
         print "Folder: " + FOLDER
         print "Files: " + str(FILES)
+        print "Glob: " + str(GLOB)
         print "Use Cache (Check MD5 Hash): " + str(CACHE)
         print "Delete after Transfer: " + str(DELETE)
         print "Location: " + LOCATION
         print "User: " +  options.user
         
     
-    return (PROJECT,LOCATION, FOLDER, FILES, DELETE, CACHE,VERBOSE,options.user,options.password)
+    return (PROJECT,LOCATION, FOLDER, FILES, GLOB, DELETE, CACHE,VERBOSE,options.user,options.password)
 
     
-def upload_files_and_folders(TC,projectId,PROJECT, folderId,FOLDER_PATH,FILES,DELETE,CACHE,RECURSE):
+def upload_files_and_folders(TC,projectId,PROJECT, folderId,FOLDER_PATH,FILES,GLOB,DELETE,CACHE,RECURSE):
 #  traceback.print_stack()
 #  pdb.set_trace()
 #  print ("{}: {}".format(projectId,PROJECT))
@@ -74,42 +83,10 @@ def upload_files_and_folders(TC,projectId,PROJECT, folderId,FOLDER_PATH,FILES,DE
   connect_folder=TC.folders_only(connect_contents)
   
 #  pprint(connect_files)
-#TODO This is not documented at all
+  if FILES ==[]: #Did not get files passed so use the GLOB to get them
+    FILES= glob.glob(GLOB)
+    
   for file in FILES: 
-    if os.path.isdir(file) and RECURSE:
-      logger.info("Directory: "+file)
-      
-      if FOLDER_PATH=="/":
-        subfolderId=TC.get_folderId_by_path(projectId,PROJECT,file)
-        new_FOLDER_PATH=file
-      else:
-        new_FOLDER_PATH=FOLDER_PATH+"/"+file
-        subfolderId=TC.get_folderId_by_path(projectId,PROJECT,new_FOLDER_PATH)
-        
-      sys.stdout.write("Directory: {}\n".format(new_FOLDER_PATH))
-        
-      if subfolderId == None:
-        subfolderId=TC.create_folder(projectId,folderId,file)
-        
-        if subfolderId == None:
-           logger.critical("could not create folder {}".format(file))
-           sys.exit("could not create folder {}".format(file))
-
-      logger.info("folderID: "+subfolderId)
-      logger.debug("Changing directory to : "+file)          
-      os.chdir(file)
-      upload_files_and_folders(TC,projectId,PROJECT,subfolderId,new_FOLDER_PATH,glob.glob("*"),DELETE,CACHE,RECURSE)
-      os.chdir("..")
-      logger.debug("Back from sub directory upload")
-      sys.stdout.write("Directory: {}\n".format(FOLDER_PATH))
-
-#      print ("{}: {}".format(projectId,PROJECT))
-#      print ("{}: {}".format(folderId,FOLDER_PATH))
-#      print os.getcwd()
-#      pprint (FILES)
-          
-
-      
     if os.path.isfile(file):
       local_filename=os.path.basename(file)
       if CACHE and (local_filename in connect_files):
@@ -132,10 +109,53 @@ def upload_files_and_folders(TC,projectId,PROJECT, folderId,FOLDER_PATH,FILES,DE
            sys.stdout.write("Uploaded. ")
       sys.stdout.write("\n")
 
+  if RECURSE:    
+    DIRS=os.listdir(".")
+  else:
+    DIRS=[]
+    
+  for dir in DIRS:     
+    if os.path.isdir(dir) and RECURSE:
+      logger.info("Directory: "+dir)
+      
+      if dir[0]==".":
+        logger.info("Skipping Directory that starts with .: {}\n".format(dir))
+        sys.stdout.write("Skipping Directory that starts with .: {}\n ".format(dir))
+        continue
+       
+      
+      if FOLDER_PATH=="/":
+        subfolderId=TC.get_folderId_by_path(projectId,PROJECT,dir)
+        new_FOLDER_PATH=dir
+      else:
+        new_FOLDER_PATH=FOLDER_PATH+"/"+dir
+        subfolderId=TC.get_folderId_by_path(projectId,PROJECT,new_FOLDER_PATH)
+        
+      sys.stdout.write("Directory: {}\n".format(new_FOLDER_PATH))
+        
+      if subfolderId == None:
+        subfolderId=TC.create_folder(projectId,folderId,dir)
+        
+        if subfolderId == None:
+           logger.critical("could not create folder {}".format(dir))
+           sys.exit("could not create folder {}".format(dir))
+
+      logger.info("folderID: "+subfolderId)
+      logger.debug("Changing directory to : "+dir)          
+      os.chdir(dir)
+      if GLOB==None: # If we did not get a GLOB passed then do all of the files in the sub folder
+        upload_files_and_folders(TC,projectId,PROJECT,subfolderId,new_FOLDER_PATH,glob.glob("*"),None,DELETE,CACHE,RECURSE)
+      else:  #Got a Glob, do not pass any files pass the GLOB 
+        upload_files_and_folders(TC,projectId,PROJECT,subfolderId,new_FOLDER_PATH,[],GLOB,DELETE,CACHE,RECURSE)
+      os.chdir("..")
+      logger.debug("Back from sub directory upload")
+      sys.stdout.write("Back from sub directoy to Directory: {}\n".format(FOLDER_PATH))
+
+      
 
 
 def main():
-  (PROJECT,LOCATION, FOLDER, FILES, DELETE, CACHE, VERBOSE,USER,PASSWORD) = process_arguments()
+  (PROJECT,LOCATION, FOLDER, FILES, GLOB, DELETE, CACHE, VERBOSE,USER,PASSWORD) = process_arguments()
   TC=Connect(USER,PASSWORD,VERBOSE)
 
   Logged_In = TC.Login()
@@ -163,7 +183,7 @@ def main():
     logger.info("folderID: "+folderId)
 
   RECURSE=True
-  upload_files_and_folders(TC,projectId,PROJECT,folderId,FOLDER,FILES,DELETE,CACHE,RECURSE)
+  upload_files_and_folders(TC,projectId,PROJECT,folderId,FOLDER,FILES,GLOB,DELETE,CACHE,RECURSE)
    
   logger.info("Logging out")
 
