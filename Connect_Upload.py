0be9b35f260911da14706e497ed0f3734bcede4e
Geoffrey Kirk <geoffrey_kirk@trimble.com>
2017-11-25 16:16:17 -0700

    Fixed Bug with second level folders not globing

--- Connect_Upload.py
+++ Connect_Upload.py
@@ -227,7 +227,7 @@ def upload_files_and_folders(TC,projectId,PROJECT, folderId,FOLDER_PATH,FILES,GL
       if GLOB==None: # If we did not get a GLOB passed then do all of the files in the sub folder
         upload_files_and_folders(TC,projectId,PROJECT,subfolderId,new_FOLDER_PATH,glob.glob("*"),None,AGE,DELETE,CACHE,RECURSE,VERBOSE)
       else:  #Got a Glob, do not pass any files pass the GLOB 
-        upload_files_and_folders(TC,projectId,PROJECT,subfolderId,new_FOLDER_PATH,[],GLOB,AGE,DELETE,CACHE,RECURSE,VERBOSE)
+        upload_files_and_folders(TC,projectId,PROJECT,subfolderId,new_FOLDER_PATH,None,GLOB,AGE,DELETE,CACHE,RECURSE,VERBOSE)
       os.chdir("..")
       logger.debug("Directory is : "+os.getcwd())          
       logger.debug("Back from sub directory upload")
